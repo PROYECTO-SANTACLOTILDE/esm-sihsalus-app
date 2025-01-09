43f9bf1c61978e56b13a1471ca43ab6f98085ad0
Gonzalo <gonzalo.galvezc@pucp.edu.pe>
2025-01-08 21:50:38 -0500

    Elimina archivos de estilos y componentes no utilizados para limpiar el código.

--- src/root.test.tsx
+++ src/root.test.tsx
@@ -22,7 +22,7 @@
  *   https://testing-library.com/docs/guiding-principles
  */
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { useConfig } from '@openmrs/esm-framework';
 import { Config } from './config-schema';
 import Root from './root.component';
@@ -41,11 +41,4 @@ it('renders a landing page for the Template app', () => {
   mockUseConfig.mockReturnValue(config);
 
   render(<Root />);
-
-  expect(screen.getByRole('heading', { name: /welcome to the o3 template app/i })).toBeInTheDocument();
-  expect(screen.getByRole('heading', { name: /configuration system/i })).toBeInTheDocument();
-  expect(screen.getByRole('heading', { name: /extension system/i })).toBeInTheDocument();
-  expect(screen.getByRole('heading', { name: /data fetching/i })).toBeInTheDocument();
-  expect(screen.getByRole('heading', { name: /resources/i })).toBeInTheDocument();
-  expect(screen.getByRole('button', { name: /get a patient named 'test'/i })).toBeInTheDocument();
 });
