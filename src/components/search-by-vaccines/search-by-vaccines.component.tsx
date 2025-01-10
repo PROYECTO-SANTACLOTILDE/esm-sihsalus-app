c70c80097de6c7267c47f061873e2117228e2482
Gonzalo <gonzalo.galvezc@pucp.edu.pe>
2025-01-09 20:54:50 -0500

    Actualiza el conjunto de conceptos de inmunización en el componente de búsqueda de vacunas para utilizar el identificador CIEL.

--- src/components/search-by-vaccines/search-by-vaccines.component.tsx
+++ src/components/search-by-vaccines/search-by-vaccines.component.tsx
@@ -10,7 +10,7 @@ interface SearchByVaccinesProps {
 }
 
 const immunizationsConfig: ImmunizationWidgetConfigObject = {
-  immunizationConceptSet: '984AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+  immunizationConceptSet: 'CIEL:984',
   sequenceDefinitions: [],
 };
 
