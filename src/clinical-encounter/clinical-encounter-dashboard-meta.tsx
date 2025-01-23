d9a62e95c81c4365a140a7e3d5e376dfbce6bfbc
Gonzalo <gonzalo.galvezc@pucp.edu.pe>
2025-01-22 21:53:40 -0500

    mostrito

---
+++ src/clinical-encounter/clinical-encounter-dashboard-meta.tsx
@@ -0,0 +1,8 @@
+export const inPatientClinicalEncounterDashboardMeta = {
+  slot: 'patient-chart-clinical-encounter-slot',
+  columns: 1,
+  title: 'Clinical Encounter',
+  path: 'clinical-encounter',
+  moduleName: '@kenyaemr/esm-patient-clinical-view-app',
+  config: {},
+};
