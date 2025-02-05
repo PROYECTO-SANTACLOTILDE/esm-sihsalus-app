161f183a6f12e82c3c9c8e9c48116be5e3521f5f
Gonzalo <gonzalo.galvezc@pucp.edu.pe>
2025-02-05 06:57:46 -0500

    .

--- src/well-child-care/wcc-dashboard.meta.tsx
+++ src/well-child-care/wcc-dashboard.meta.tsx
@@ -1,37 +1,36 @@
-export const antenatalDashboardMeta = {
-  icon: 'antenatal-care',
-  slot: 'patient-chart-antenatal-dashboard-slot',
+export const childImmunizationScheduleDashboardMeta = {
+  icon: 'child-immunization',
+  slot: 'patient-chart-child-immunization-schedule-slot',
   columns: 1,
-  title: 'Antenatal Care',
-  path: 'antenatal-care-dashboard',
-  moduleName: '@kenyaemr/esm-patient-clinical-view-app',
+  title: 'Child Immunization Schedule',
+  path: 'child-immunization-schedule-dashboard',
+  moduleName: '@Duvet05/esm-patient-clinical-view-app',
   config: {},
 };
 
-export const postnatalDashboardMeta = {
-  icon: 'postnatalDashboardMeta',
-  slot: 'patient-chart-postnatal-dashboard-slot',
+export const neonatalCareDashboardMeta = {
+  icon: 'neonatal-care',
+  slot: 'patient-chart-neonatal-care-slot',
   columns: 1,
-  title: 'Postnatal Care',
-  path: 'postnatal-care-dashboard',
-  moduleName: '@kenyaemr/esm-patient-clinical-view-app',
+  title: 'Neonatal Care',
+  path: 'neonatal-care-dashboard',
+  moduleName: '@Duvet05/esm-patient-clinical-view-app',
   config: {},
 };
 
-export const labourAndDeliveryDashboardMeta = {
-  icon: 'labourAndDeliveryDashboardMeta',
-  slot: 'patient-chart-labour-and-delivery-dashboard-slot',
+export const wellChildCareDashboardMeta = {
+  icon: 'well-child-care',
+  slot: 'patient-chart-well-child-care-slot',
   columns: 1,
-  title: 'Labour & Delivery',
-  path: 'labour-and-delivery-dashboard',
-  moduleName: '@kenyaemr/esm-patient-clinical-view-app',
+  title: 'Well Child Care',
+  path: 'well-child-care-dashboard',
+  moduleName: '@Duvet05/esm-patient-clinical-view-app',
   config: {},
 };
 
-export const maternalAndChildHealthNavGroup = {
-  title: 'Madre Gestante',
-  slotName: 'maternal-and-child-health-slot',
+export const wellChildCareNavGroup = {
+  title: 'Control de Crecimiento y Desarrollo',
+  slotName: 'well-child-care-slot',
   isExpanded: false,
-  showWhenExpression:
-    'patient.gender === "female" && (enrollment.includes("MCH - Child Services") || enrollment.includes("MCH - Mother Services"))',
+  showWhenExpression: 'enrollment.includes("CRED - Child Services")',
 };
