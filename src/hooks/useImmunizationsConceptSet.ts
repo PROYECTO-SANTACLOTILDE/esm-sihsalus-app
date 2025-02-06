dd7dc852f8ffa4a33f6d85292e391b0f25736741
Gonzalo <gonzalo.galvezc@pucp.edu.pe>
2025-02-06 03:07:20 -0500

    Agregar manejo de errores y mejoras en la gestión de vacunas en el componente ImmunizationPlanBuilder, incluyendo la visualización de notificaciones y la edición de dosis.

--- src/hooks/useImmunizationsConceptSet.ts
+++ src/hooks/useImmunizationsConceptSet.ts
@@ -2,7 +2,7 @@ import { openmrsFetch, restBaseUrl } from '@openmrs/esm-framework';
 import type { ImmunizationWidgetConfigObject,  OpenmrsConcept } from '../immunization-plan/fhir-immunization-domain';
 import useSWR from 'swr';
 
-export function useImmunizationsConceptSet(config: ImmunizationWidgetConfigObject): { immunizationsConceptSet: OpenmrsConcept | undefined; isLoading: boolean } {
+export function useImmunizationsConceptSet(config: ImmunizationWidgetConfigObject): { immunizationsConceptSet: OpenmrsConcept | undefined; isLoading: boolean; error: Error | undefined } {
   const conceptRepresentation =
     'custom:(uuid,display,answers:(uuid,display),conceptMappings:(conceptReferenceTerm:(conceptSource:(name),code)))';
 
@@ -13,5 +13,6 @@ export function useImmunizationsConceptSet(config: ImmunizationWidgetConfigObjec
   return {
     immunizationsConceptSet: data && data.data.results[0],
     isLoading,
+    error
   };
 }
