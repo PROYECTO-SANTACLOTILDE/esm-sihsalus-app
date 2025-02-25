b4d1e408be682a1b7de1b316e9d8e7c8080c3510
Gonzalo <gonzalo.galvezc@pucp.edu.pe>
2025-02-25 15:07:49 -0500

    feat: agregar nuevas constantes para el formulario de entrega en el módulo de CRED

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -57,8 +57,10 @@ export const GestationalSize_UUID = '1789AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
 export const BirthAbnormally_UUID = '164122AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
 export const BloodLoss_UUID = '161928AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
 export const GivenVitaminK_UUID = '984AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
+export const DeliveryForm_UUID = 'Gonzalo';
+export const Form_NOMBRE = 'Sample Form';
 
-// 9. CRED
+// 0. CRED
 export const Pagina_10_Alojamiento_Conjunto = "Pagina_10_Alojamiento_Conjunto";
 export const Pagina_11_Y_12_Valoracion_De_Enfermeria = "Pagina_11_Y_12_Valoracion_De_Enfermeria";
 export const Pagina_13_Ordenes_Medicas = "Pagina_13_Ordenes_Medicas";
@@ -80,8 +82,6 @@ export const Pagina_30_31_32_Y_33_Eedp_2_Meses = "Pagina_30_31_32_Y_33_Eedp_2_Me
 export const Pagina_30_31_32_Y_33_Eedp_21_Meses = "Pagina_30_31_32_Y_33_Eedp_21_Meses";
 export const Pagina_30_31_32_Y_33_Eedp_5_Meses = "Pagina_30_31_32_Y_33_Eedp_5_Meses";
 export const Pagina_30_31_32_Y_33_Eedp_8_Meses = "Pagina_30_31_32_Y_33_Eedp_8_Meses";
-
-
 
 // 10. PARTOGRAPHY
 export const Progress_UUID = '160116AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
