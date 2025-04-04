6f0748076178421eb1ee69da8cd5bc5cd80a392c
Gonzalo <gonzalo.galvezc@pucp.edu.pe>
2025-04-04 06:24:18 -0500

    refactor: actualizar etiquetas en el componente de resumen de historial de parto

--- src/well-child-care/components/neonatal-register/labour-history/labour-history.component.tsx
+++ src/well-child-care/components/neonatal-register/labour-history/labour-history.component.tsx
@@ -156,7 +156,7 @@ const LabourHistorySummary: React.FC<LabourHistorySummaryProps> = ({ patientUuid
         <DataTable rows={summaryRows} headers={headers} size="sm" useZebraStyles>
           {({ rows, headers, getHeaderProps, getTableProps }) => (
             <TableContainer>
-              <Table {...getTableProps()} aria-label="Labour History Summary">
+              <Table {...getTableProps()} aria-label="Detalles del Parto">
                 <TableHead>
                   <TableRow>
                     {headers.map((header) => (
@@ -180,7 +180,7 @@ const LabourHistorySummary: React.FC<LabourHistorySummaryProps> = ({ patientUuid
         </DataTable>
       ) : (
         <EmptyState
-          headerTitle={t('labourHistorySummary', 'Labour History Summary')}
+          headerTitle={t('labourHistorySummary', 'Detalles del Parto')}
           displayText={t('noDataAvailableDescription', 'No data available')}
           launchForm={handleAddLabourDetails}
         />
