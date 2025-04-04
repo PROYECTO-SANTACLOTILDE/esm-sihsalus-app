0295e0afbc5618727c3996d488bda5d158a39617
Gonzalo <gonzalo.galvezc@pucp.edu.pe>
2025-04-04 03:21:54 -0500

    version generica lista!

--- src/well-child-care/components/newborn-monitoring/newborn balance/balance-overview.component.tsx
+++ src/well-child-care/components/newborn-monitoring/newborn balance/balance-overview.component.tsx
@@ -2,9 +2,7 @@ import React, { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useConfig } from '@openmrs/esm-framework';
 import { useBalance, useVitalsConceptMetadata, withUnit } from '../../../common';
-import ClinicalDataOverview from '../clinical-data-overview.component';
-import { type ClinicalField } from '../clinical-data-overview.component';
-
+import ClinicalDataOverview from '../../../../ui/data-table/clinical-data-overview.component';
 import { formatDate, parseDate } from '@openmrs/esm-framework';
 import styles from './balance-overview.scss';
 
@@ -13,67 +11,78 @@ interface BalanceOverviewProps {
   pageSize?: number;
 }
 
-const clinicalFields: ClinicalField[] = [
-  {
-    key: 'stoolCount',
-    conceptUuid: 'stoolCountUuid',
-    label: 'stoolCount',
-    isSortable: true,
-    sortFunc: (a, b) => (a.stoolCount && b.stoolCount ? a.stoolCount - b.stoolCount : 0),
-    showInChart: true,
-  },
-  {
-    key: 'stoolGrams',
-    conceptUuid: 'stoolGramsUuid',
-    label: 'stoolGrams',
-    isSortable: true,
-    sortFunc: (a, b) => (a.stoolGrams && b.stoolGrams ? a.stoolGrams - b.stoolGrams : 0),
-    showInChart: true,
-  },
-  {
-    key: 'urineCount',
-    conceptUuid: 'urineCountUuid',
-    label: 'urineCount',
-    isSortable: true,
-    sortFunc: (a, b) => (a.urineCount && b.urineCount ? a.urineCount - b.urineCount : 0),
-    showInChart: true,
-  },
-  {
-    key: 'urineGrams',
-    conceptUuid: 'urineGramsUuid',
-    label: 'urineGrams',
-    isSortable: true,
-    sortFunc: (a, b) => (a.urineGrams && b.urineGrams ? a.urineGrams - b.urineGrams : 0),
-    showInChart: true,
-  },
-  {
-    key: 'vomitCount',
-    conceptUuid: 'vomitCountUuid',
-    label: 'vomitCount',
-    isSortable: true,
-    sortFunc: (a, b) => (a.vomitCount && b.vomitCount ? a.vomitCount - b.vomitCount : 0),
-    showInChart: true,
-  },
-  {
-    key: 'vomitGramsML',
-    conceptUuid: 'vomitGramsMLUuid',
-    label: 'vomitGramsML',
-    isSortable: true,
-    sortFunc: (a, b) => (a.vomitGramsML && b.vomitGramsML ? a.vomitGramsML - b.vomitGramsML : 0),
-    showInChart: true,
-  },
-];
-
 const NewbornBalanceOverview: React.FC<BalanceOverviewProps> = ({ patientUuid, pageSize = 10 }) => {
   const { t } = useTranslation();
   const config = useConfig();
   const { data: conceptUnits } = useVitalsConceptMetadata();
   const { data: balanceData, error, isLoading, isValidating } = useBalance(patientUuid);
 
+  const clinicalFields = useMemo(
+    () => [
+      {
+        key: 'date',
+        label: 'dateAndTime',
+        isSortable: true,
+        sortFunc: (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime(),
+        format: (date) => formatDate(parseDate(date), { mode: 'wide', time: true }),
+        showInChart: false,
+      },
+      {
+        key: 'stoolCount',
+        conceptUuid: 'stoolCountUuid',
+        label: 'stoolCount',
+        isSortable: true,
+        sortFunc: (a, b) => (a.stoolCount && b.stoolCount ? a.stoolCount - b.stoolCount : 0),
+        showInChart: true,
+      },
+      {
+        key: 'stoolGrams',
+        conceptUuid: 'stoolGramsUuid',
+        label: 'stoolGrams',
+        isSortable: true,
+        sortFunc: (a, b) => (a.stoolGrams && b.stoolGrams ? a.stoolGrams - b.stoolGrams : 0),
+        showInChart: true,
+      },
+      {
+        key: 'urineCount',
+        conceptUuid: 'urineCountUuid',
+        label: 'urineCount',
+        isSortable: true,
+        sortFunc: (a, b) => (a.urineCount && b.urineCount ? a.urineCount - b.urineCount : 0),
+        showInChart: true,
+      },
+      {
+        key: 'urineGrams',
+        conceptUuid: 'urineGramsUuid',
+        label: 'urineGrams',
+        isSortable: true,
+        sortFunc: (a, b) => (a.urineGrams && b.urineGrams ? a.urineGrams - b.urineGrams : 0),
+        showInChart: true,
+      },
+      {
+        key: 'vomitCount',
+        conceptUuid: 'vomitCountUuid',
+        label: 'vomitCount',
+        isSortable: true,
+        sortFunc: (a, b) => (a.vomitCount && b.vomitCount ? a.vomitCount - b.vomitCount : 0),
+        showInChart: true,
+      },
+      {
+        key: 'vomitGramsML',
+        conceptUuid: 'vomitGramsMLUuid',
+        label: 'vomitGramsML',
+        isSortable: true,
+        sortFunc: (a, b) => (a.vomitGramsML && b.vomitGramsML ? a.vomitGramsML - b.vomitGramsML : 0),
+        showInChart: true,
+      },
+    ],
+    [t],
+  );
+
   const { tableHeaders, tableRows, chartConfig } = useMemo(() => {
     // Generar tableHeaders
     const headers = clinicalFields.map((field) => ({
-      key: `${field.key}Render`,
+      key: field.key,
       header: field.conceptUuid
         ? withUnit(t(field.label), conceptUnits.get(config.concepts[field.conceptUuid]) ?? '')
         : t(field.label),
@@ -82,17 +91,14 @@ const NewbornBalanceOverview: React.FC<BalanceOverviewProps> = ({ patientUuid, p
     }));
 
     // Generar tableRows
-    const rows = balanceData?.map((item, index) => {
-      const row = { id: `${index}` };
-      clinicalFields.forEach((field) => {
-        if (field.key === 'date') {
-          row['dateRender'] = formatDate(parseDate(item.date.toString()), { mode: 'wide', time: true });
-        } else {
-          row[`${field.key}Render`] = item[field.key] ?? '--';
-        }
-      });
-      return row;
-    });
+    const rows =
+      balanceData?.map((item, index) => {
+        const row = { id: `${index}` };
+        clinicalFields.forEach((field) => {
+          row[field.key] = field.format ? field.format(item[field.key] || item.date) : (item[field.key] ?? '--');
+        });
+        return row;
+      }) || [];
 
     // Generar chartConfig
     const vitalSigns = clinicalFields
@@ -105,13 +111,13 @@ const NewbornBalanceOverview: React.FC<BalanceOverviewProps> = ({ patientUuid, p
 
     return {
       tableHeaders: headers,
-      tableRows: rows || [],
+      tableRows: rows,
       chartConfig: {
         vitalSigns,
         mappings: {},
       },
     };
-  }, [t, config.concepts, conceptUnits, balanceData]);
+  }, [clinicalFields, balanceData, conceptUnits, config.concepts, t]);
 
   return (
     <ClinicalDataOverview
