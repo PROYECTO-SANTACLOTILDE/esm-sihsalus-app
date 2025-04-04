544f1f2fe1116e4e6b69c5f7cd486726c24e1d38
Gonzalo <gonzalo.galvezc@pucp.edu.pe>
2025-04-04 01:29:55 -0500

    agregar componente TabbedDashboard y refactorizar NeonatalCare para utilizarlo

--- src/well-child-care/neonatal-care.component.tsx
+++ src/well-child-care/neonatal-care.component.tsx
@@ -1,103 +1,39 @@
-import React, { useMemo } from 'react';
-import classNames from 'classnames';
-import { useTranslation } from 'react-i18next';
-import { Extension, ExtensionSlot, useExtensionSlotMeta } from '@openmrs/esm-framework';
+import React from 'react';
 import { Activity, CloudMonitoring, WatsonHealthCobbAngle, UserFollow, Stethoscope } from '@carbon/react/icons';
-import { Layer, Tab, TabList, TabPanel, TabPanels, Tabs, Tile } from '@carbon/react';
+import TabbedDashboard from '../ui/tabbed-dashboard/tabbed-dashboard.component'; // Default import
+import { TabConfig } from '../ui/tabbed-dashboard/tabbed-dashboard.component'; // Named type import
 
-import styles from './well-child-care.scss';
-
-interface NeonatalCareProps {
-  patientUuid: string;
-}
-
-const NeonatalCare: React.FC<NeonatalCareProps> = ({ patientUuid }) => {
-  const { t } = useTranslation();
-
-  const tabs = useMemo(
-    () => [
-      {
-        label: t('vitalsNewborn', 'Monitoreo del Recién Nacido'),
-        icon: Activity,
-        slotName: 'neonatal-vitals-slot',
-      },
-      {
-        label: t('perinatal', 'Inscripción Materno Perinatal'),
-        icon: UserFollow,
-        slotName: 'neonatal-perinatal-slot',
-      },
-      {
-        label: t('atencionInmediata', 'Atención Inmediata del RN'),
-        icon: CloudMonitoring,
-        slotName: 'neonatal-attention-slot',
-      },
-      {
-        label: t('evaluacionInmediata', 'Evaluación del Recién Nacido'),
-        icon: Stethoscope,
-        slotName: 'neonatal-evaluation-slot',
-      },
-      {
-        label: t('consejeriaLactancia', 'Consejería Lactancia Materna'),
-        icon: WatsonHealthCobbAngle,
-        slotName: 'neonatal-counseling-slot',
-      },
-    ],
-    [t],
-  );
-
-  // Get meta data for all slots at the top level
-  const slotMetas = {
-    'neonatal-vitals-slot': useExtensionSlotMeta('neonatal-vitals-slot'),
-    'neonatal-perinatal-slot': useExtensionSlotMeta('neonatal-perinatal-slot'),
-    'neonatal-attention-slot': useExtensionSlotMeta('neonatal-attention-slot'),
-    'neonatal-evaluation-slot': useExtensionSlotMeta('neonatal-evaluation-slot'),
-    'neonatal-counseling-slot': useExtensionSlotMeta('neonatal-counseling-slot'),
-  };
+const NeonatalCare: React.FC<{ patientUuid: string }> = ({ patientUuid }) => {
+  const tabs: TabConfig[] = [
+    {
+      labelKey: 'vitalsNewborn',
+      icon: Activity,
+      slotName: 'neonatal-vitals-slot',
+    },
+    {
+      labelKey: 'perinatal',
+      icon: UserFollow,
+      slotName: 'neonatal-perinatal-slot',
+    },
+    {
+      labelKey: 'atencionInmediata',
+      icon: CloudMonitoring,
+      slotName: 'neonatal-attention-slot',
+    },
+    {
+      labelKey: 'evaluacionInmediata',
+      icon: Stethoscope,
+      slotName: 'neonatal-evaluation-slot',
+    },
+    {
+      labelKey: 'consejeriaLactancia',
+      icon: WatsonHealthCobbAngle,
+      slotName: 'neonatal-counseling-slot',
+    },
+  ];
 
   return (
-    <div className={styles.widgetCard}>
-      <Layer>
-        <Tile>
-          <div className={styles.desktopHeading}>
-            <h4>{t('neonatalCare', 'Cuidado del Recién Nacido')}</h4>
-          </div>
-        </Tile>
-      </Layer>
-
-      <Layer>
-        <Tabs>
-          <TabList
-            className={styles.tabList}
-            aria-label={t('neonatalCareTabs', 'Lista de pestañas de cuidado neonatal')}
-          >
-            {tabs.map((tab, index) => (
-              <Tab className={styles.tab} key={index} renderIcon={tab.icon}>
-                {tab.label}
-              </Tab>
-            ))}
-          </TabList>
-
-          <TabPanels>
-            {tabs.map((tab, index) => (
-              <TabPanel key={index} className={styles.dashboardContainer}>
-                <div className={styles.dashboardContainer}>
-                  <ExtensionSlot key={tab.slotName} name={tab.slotName} className={styles.dashboard}>
-                    {(extension) => {
-                      const { fullWidth = false } = slotMetas[tab.slotName][extension.id] || {};
-                      return (
-                        <div className={classNames(styles.extension, fullWidth && styles.fullWidth)}>
-                          <Extension state={{ patientUuid, pageSize: 5 }} className={styles.extensionWrapper} />
-                        </div>
-                      );
-                    }}
-                  </ExtensionSlot>
-                </div>
-              </TabPanel>
-            ))}
-          </TabPanels>
-        </Tabs>
-      </Layer>
-    </div>
+    <TabbedDashboard patientUuid={patientUuid} titleKey="neonatalCare" tabs={tabs} ariaLabelKey="neonatalCareTabs" />
   );
 };
 
