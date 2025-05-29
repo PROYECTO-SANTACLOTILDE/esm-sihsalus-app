7c383effdce2e6c6455963f2e934ed1cd9abeccd
Gonzalo <gonzalo.galvezc@pucp.edu.pe>
2025-05-29 11:48:56 -0500

    feat: Añadir scripts de despliegue y mejorar configuración de linting en package.json; renombrar archivo de estilos en CREDControlsForm

--- src/well-child-care/workspace/well-child-control/well-child-controls-form.workspace.tsx
+++ src/well-child-care/workspace/well-child-control/well-child-controls-form.workspace.tsx
@@ -32,7 +32,7 @@ import {
 } from '@openmrs/esm-framework';
 import type { DefaultPatientWorkspaceProps } from '@openmrs/esm-patient-common-lib';
 import type { ConfigObject } from '../../../config-schema';
-import styles from './cred-controls-form.scss';
+import styles from './well-child-controls-form.scss';
 
 // Validation schema with zod
 const CREDControlsSchema = z.object({
