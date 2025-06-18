14d3dfb859b405805d534d936577984184c71d3f
Gonzalo <gonzalo.galvezc@pucp.edu.pe>
2025-06-18 11:27:58 -0500

    fix: Eliminar configuración predeterminada de Dyaku en el esquema de configuración

--- src/immunization-plan/testeo.component.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import DyakuPatientsTable from '../dyaku-patients/dyaku-patients-table.component';
-
-const DyakuPatientsPage: React.FC = () => {
-  return <DyakuPatientsTable pageSize={20} />;
-};
-
-export default DyakuPatientsPage;
